ea10bfea7a9ff29cd2c1664136a965e80ce8a40f
Sinhuè Angelo Rossi <sinhue.rossi@gmail.com>
2022-12-08 18:03:59 +0100

    aggiunto mdx

--- rocket-coffee/src/components/atoms/Button/Button.stories.js
+++ rocket-coffee/src/components/atoms/Button/Button.stories.js
@@ -5,6 +5,24 @@ import Button from "./Button";
 export default {
     title: 'Atoms/Button',
     decorators: [withKnobs],
+    component: Button,
+    argTypes: {
+        children: {
+            description: 'The element that should be rendered inside the button',
+            default: 'undefined'
+        },
+        onClick: {
+            description: 'The function that should be called when the button is clicked',
+            default: 'undefined'
+        },
+    },
+    parameters: {
+        docs: {
+            description: {
+                component: 'A simple button component that can be used to trigger actions.'
+            }
+        }
+    }
 }
 
 export const standard = () => (
